c9672c7f022a3a47da5b3194ae66d410f190170a
lisaschiffli <lisaschiffli@users.noreply.github.com>
2023-08-30 13:16:05 -0400

    Finished workshop

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,8 +4,31 @@ import styled from 'styled-components/macro';
 const Hero = () => {
   return (
     <Wrapper>
-      <HeroImage src="/images/hero-img.jpg" />
-      <Swoop src="/swoop.svg" />
+      <HeroImage>
+          <source
+            type="image/avif"
+            srcSet="
+              /images/hero-img.avif 1x,
+              /images/hero-img@2x.avif 2x,
+              /images/hero-img@3x.avif 3x
+            "  
+          />
+          <source
+            type="image/jpg"
+            srcSet="
+              images/hero-img.jpg 1x,
+              images/hero-img@2x.jpg 2x,
+              images/hero-img@3x.jpg 3x
+            "
+          />
+          <img
+            alt="Example, cat on black background"
+            src="/images/hero-img.jpg"
+          />
+        </HeroImage>
+      <Swoop 
+        alt=""
+        src="/swoop.svg" />
     </Wrapper>
   );
 };
@@ -20,7 +43,7 @@ const Wrapper = styled.section`
   background: hsl(0deg 0% 1%);
 `;
 
-const HeroImage = styled.img`
+const HeroImage = styled.picture`
   display: block;
   width: 500px;
   height: 500px;
